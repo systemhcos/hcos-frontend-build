239aad5d89670c958f016209ac760482157f170d
Ian <ian@hcos.io>
2026-09-20 13:57:33 -0300

    updated balance preferences styles

--- dist/assets/TemplatePage-a_suJEYO.js
+++ dist/assets/TemplatePage-a_suJEYO.js
@@ -1,4 +1,4 @@
-import{_ as E,c as j,k as p,a as w,F as N,r as R,l as C,w as _,m as q,aM as T,s as A,f as B,t as M,g as F,i as L,p as S,o as O,ah as W,aF as D,e as H}from"./index-D0EByaNs.js";import{r as h}from"./requestBridge-Cx8b9euE.js";import{u as I}from"./useApiClient-BeCjylrQ.js";function $(b,y={},t=1e3){return new Promise((o,d)=>{const l=`
+import{_ as E,c as j,k as p,a as w,F as N,r as R,l as C,w as _,m as q,aM as T,s as A,f as B,t as M,g as F,i as L,p as S,o as O,ah as W,aF as D,e as H}from"./index-BstacYQj.js";import{r as h}from"./requestBridge-C6qhZtPj.js";import{u as I}from"./useApiClient-BvBA-eBP.js";function $(b,y={},t=1e3){return new Promise((o,d)=>{const l=`
       // Remove dangerous globals
       self.fetch = undefined;
       self.importScripts = undefined;
