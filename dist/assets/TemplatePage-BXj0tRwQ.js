da8f7f043529ea3e34a60bfb26dfc11993837e86
Ian <ian@hcos.io>
2026-09-23 01:42:30 -0300

    refactored balance topup in hosting component v1

--- dist/assets/TemplatePage-BXj0tRwQ.js
+++ dist/assets/TemplatePage-BXj0tRwQ.js
@@ -1,4 +1,4 @@
-import{_ as E,c as j,k as p,a as w,F as N,r as R,l as C,w as _,m as q,aM as T,s as A,f as B,t as M,g as F,i as L,p as S,o as O,ah as W,aF as D,e as H}from"./index-BnYmFTJ1.js";import{r as h}from"./requestBridge-BZToOYCM.js";import{u as I}from"./useApiClient-Bohk-dmh.js";function $(b,y={},t=1e3){return new Promise((o,d)=>{const l=`
+import{_ as E,c as j,k as p,a as w,F as N,r as R,l as C,w as _,m as q,aM as T,s as A,f as B,t as M,g as F,i as L,p as S,o as O,ah as W,aF as D,e as H}from"./index-CPDtFyou.js";import{r as h}from"./requestBridge-Dv0RmV0c.js";import{u as I}from"./useApiClient-DWLb1Y3i.js";function $(b,y={},t=1e3){return new Promise((o,d)=>{const l=`
       // Remove dangerous globals
       self.fetch = undefined;
       self.importScripts = undefined;
